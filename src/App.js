edbea846085f79523a998be6ef1ce4779c015a56
lleach1 <lleach1@swarthmore.edu>
2017-04-08 08:09:00 -0400

    sending raw HTML for result row (may need a table)

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { SearchkitManager,SearchkitProvider,
   SearchBox, RefinementListFilter, Pagination,
   HierarchicalMenuFilter, HitsStats, SortingSelector, NoHits,
   ResetFilters, RangeFilter, NumericRefinementListFilter,
-  ViewSwitcherHits, ViewSwitcherToggle, DynamicRangeFilter,
+  Hits, DynamicRangeFilter,
   InputFilter, GroupedSelectedFilters,
   Layout, TopBar, LayoutBody, LayoutResults,
   ActionBar, ActionBarRow, SideBar, Select } from 'searchkit'
@@ -16,15 +16,18 @@ const searchkit = new SearchkitManager(host, {
   searchUrlPath: "/" + indexName + "/_search"
 })
 
-
 const CourseListItem = (props)=> {
   const {bemBlocks, result} = props
-  //let url = "http://www.imdb.com/title/" + result._source.imdbId
-  //const source:any = extend({}, result._source, result.highlight)
-  let raw = JSON.stringify(result);
+  const source:any = extend({}, result._source, result.highlight)
+  let raw = JSON.stringify(result) + "\n\n----\n" + JSON.stringify(source) + "\n\n----\n" + JSON.stringify(props);
   return (
     <div className={bemBlocks.item().mix(bemBlocks.container("item"))} data-qa="hit">
-      {raw}
+      <div className="course-campus">{source.campus}</div>
+      <div className="course-semester">{source.semester}</div>
+      <div className="course-id">{source.courseNumber}</div>
+      <div className="course-name" dangerouslySetInnerHTML={{__html: source.name}}></div>
+      <div className="course-instructor" dangerouslySetInnerHTML={{__html: source.instructor}}></div>
+      <div className="course-times">{source.day}{source.time}</div>
     </div>
   )
 }
@@ -35,7 +38,6 @@ class App extends Component {
       <SearchkitProvider searchkit={searchkit}>
         <Layout>
           <TopBar>
-            <div className="my-logo">PH</div>
             <SearchBox autofocus={true} searchOnChange={true} prefixQueryOptions={{analyzer:"standard"}} prefixQueryFields={["name^9","description^5","instructor^4","program"]}/>
           </TopBar>
 
@@ -63,13 +65,7 @@ class App extends Component {
               </ActionBarRow>
 
             </ActionBar>
-            <ViewSwitcherHits
-                hitsPerPage={20} highlightFields={["name","description"]}
-                hitComponents={[
-                  {key:"list", title:"List", itemComponent:CourseListItem, defaultOption:true}
-                ]}
-                scrollTo="body"
-            />
+            <Hits hitsPerPage={20} highlightFields={["name","description","instructor"]} itemComponent={CourseListItem} scrollTo="body" mod="sk-hits-list" />
             <NoHits suggestionsField={"name"}/>
             <Pagination showNumbers={true}/>
           </LayoutResults>
